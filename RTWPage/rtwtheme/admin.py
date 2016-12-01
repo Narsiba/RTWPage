160303b80cd95919f8aff70294594c6c10a841da
Sab Gardner <gardnersab@gmail.com>
2016-12-01 09:59:15 +0100

    RTWPage, first commit

---
+++ RTWPage/rtwtheme/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from mezzanine.core.admin import TabularDynamicInlineAdmin
+from mezzanine.pages.admin import PageAdmin
+from .models import HomePage, Slide, Portfolio
+
+# Register your models here.
+class SlideInline(TabularDynamicInlineAdmin):
+    model = Slide
+
+#class IconInline(TabularDynamicInlineAdmin):
+#    model = IconBlurb
+
+class HomePageAdmin(PageAdmin):
+    model = HomePage
+    inlines = [
+        SlideInline,
+    ]
+
+admin.site.register(HomePage, HomePageAdmin)
+admin.site.register(Portfolio, PageAdmin)
