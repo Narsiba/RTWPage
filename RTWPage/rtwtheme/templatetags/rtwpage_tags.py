bc634be792bbebc337534d6ba3e15157eb9fa98d
Sab Gardner <gardnersab@gmail.com>
2017-01-22 19:59:45 +0700

    Blog set up

--- RTWPage/rtwtheme/templatetags/rtwpage_tags.py
+++ RTWPage/rtwtheme/templatetags/rtwpage_tags.py
@@ -1,3 +1,10 @@
 from django import template
+from mezzanine.blog.models import BlogPost, BlogCategory
 
 register = template.Library()
+
+@register.filter(name='has_category')
+def has_category(blog_post):
+    for category in categories:
+        if category.title == 'Laos':
+            return blog_post
