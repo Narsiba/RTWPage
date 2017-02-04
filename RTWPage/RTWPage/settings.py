df301d97a5e5f3701a7fa3f3042c6c0646e0a03f
Sab Gardner <gardnersab@gmail.com>
2017-02-04 20:51:34 +0700

    additional files and changes for Heroku

--- RTWPage/RTWPage/settings.py
+++ RTWPage/RTWPage/settings.py
@@ -341,3 +341,18 @@ except ImportError:
     pass
 else:
     set_dynamic_settings(globals())
+
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
--- RTWPage/RTWPage/wsgi.py
+++ RTWPage/RTWPage/wsgi.py
@@ -16,3 +16,6 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "%s.settings" % real_project_name("RTWPage"))
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
